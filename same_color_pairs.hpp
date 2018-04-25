9c56d227488ca3ea2ded74139988da53babbba5e
nel215 <otomo.yuhei@gmail.com>
2018-04-25 22:51:42 +0900

    Fix timelimit

--- same_color_pairs.hpp
+++ same_color_pairs.hpp
@@ -20,7 +20,7 @@ const double ticks_per_sec = 3200000000;
 const double timeLimit = 6.0;
 #else
 const double ticks_per_sec = 3000000000;
-const double timeLimit = 9.15;
+const double timeLimit = 9.10;
 #endif  // LOCAL
 inline double getTime() {
     uint32_t lo, hi;
